b8874123156b422582b50da24f0945e86017a3b7
Adrian Garcia <AdrianG@spacebarmedia.com>
2018-09-27 12:22:30 +0100

    Examples

---
+++ js/callback.js
@@ -0,0 +1,39 @@
+
+var button2 = document.getElementById('example2');
+var text2 = document.getElementById('example2_text');
+
+var car = {
+    model1: 'BMW',
+    owner1: 'Adrian',
+    km1: '100000',
+    printInformation:  function () {
+        var text = this.owner1 + ' has a ' + this.model1 + ' with ' + this.km1 + ' km';
+        console.log(text);
+        text2.innerHTML += text;
+    }
+}
+
+// this would take the context of the button object
+//button2.addEventListener('click', car.printInformation) // undefined has a undefined with undefined
+
+// We need to bind the callback method with the this context of the car
+button2.addEventListener('click', car.printInformation.bind(car)) // Adrian has a BMW with 100000
+
+
+// If we define the prinInformation method with arrow function, the this takes the context where it's been 
+// defined, hence, this points to window
+
+// var car = {
+//     printInformation: () => {
+//         var text = this.owner1 + ' has a ' + this.model1 + ' with ' + this.km1;
+//         console.log(this.owner1 + ' has a ' + this.model1 + ' with ' + this.km1);
+//         text2.innerHTML += text;
+//     }
+// }
+
+// So when we use the method as a callback function, it will try to get this.owner/this.model1/this.km1 from the window object
+// button2.addEventListener('click', car.printInformation)  // undefined has a undefined with undefined
+// button2.addEventListener('click', car.printInformation.bind(car)) // Still undefined has a undefined with undefined
+
+
+
---
+++ js/global.js
@@ -0,0 +1,57 @@
+
+var button1 = document.getElementById('example1');
+var text1 = document.getElementById('example1_text');
+
+var button12 = document.getElementById('example12');
+var text12 = document.getElementById('example12_text');
+
+// Global variables
+var model = 'MERCEDES',
+    owner = 'PEPE',
+    km = '100';
+
+var car = {
+    model: 'BMW',
+    owner: 'Adrian',
+    km: '100000',
+    printInformation: function () {
+        var text = this.owner + ' has a ' + this.model + ' with ' + this.km + ' km';
+        console.log(text);
+        return text;
+    }
+}
+
+function printInformation() {
+    var text = this.owner + ' has a ' + this.model + ' with ' + this.km + ' km';
+    console.log(text);
+    return text;
+}
+
+// This get the context of the object car
+var output1 = car.printInformation(); // Adrian has a BMW with 100000
+
+// It's the same than window.printInformation()
+var output12 = printInformation(); // PEPE has a MERCEDES with 100 
+
+
+
+button1.addEventListener('click', function () {
+    text1.innerHTML += output1;
+})
+button12.addEventListener('click', function () {
+    text12.innerHTML += output12;
+})
+
+// We would get an error if we used this.text1.innerHTML += output 
+// In that case this refers to the global button object 
+
+    // button.addEventListener('click', function () {
+    //     this.text1.innerHTML += output; // Error. Cannot find innerHTML of undefined
+    // })
+
+// However using arrow functions that would not be a problem. Arrow function
+// fix the 'this' to the context where it's been defined
+
+    // button.addEventListener('click', () => {
+    //     this.text1.innerHTML += output;  // Adrian has a BMW with 10000
+    // })
